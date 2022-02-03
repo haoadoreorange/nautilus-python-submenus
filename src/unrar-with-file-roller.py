9844cc74ca4fb9791eef2ca6bc1aed3395b1e62b
haoadoreorange <github@h4o.dev>
2022-02-03 12:09:39 -0500

    Fix shell=True security issue
    Signed-off-by: haoadoreorange <github@h4o.dev>

--- src/unrar-with-file-roller.py
+++ src/unrar-with-file-roller.py
@@ -2,7 +2,7 @@ from gi import require_version
 require_version('Gtk', '3.0')
 require_version('Nautilus', '3.0')
 from gi.repository import Nautilus, GObject
-from subprocess import call
+from subprocess import Popen
 import os
 
 # path to file-roller
@@ -10,29 +10,28 @@ FILE_ROLLER = 'file-roller'
 
 class UnrarExtension(GObject.GObject, Nautilus.MenuProvider):
 
-    def unrar_here(self, menu, safepath):
-        call(FILE_ROLLER + ' -h ' + safepath, shell=True)
+    def unrar_here(self, menu, filepath):
+        Popen([FILE_ROLLER, '-h', filepath])
         
-    def unrar_to(self, menu, safepath):
-        call(FILE_ROLLER + ' -f ' + safepath, shell=True) 
+    def unrar_to(self, menu, filepath):
+        Popen([FILE_ROLLER, '-f', filepath])
 
     def get_file_items(self, window, files):
         if len(files) == 1:
             filepath = files[0].get_location().get_path()
             if os.path.splitext(filepath)[1] == '.rar':
-                safepath = '"' + filepath + '"'
                 item1 = Nautilus.MenuItem(
                     name='UnrarHere',
                     label='Unrar Here',
                     tip='Unrar in the current directory'
                 )
-                item1.connect('activate', self.unrar_here, safepath)
+                item1.connect('activate', self.unrar_here, filepath)
                 item2 = Nautilus.MenuItem(
                     name='UnrarTo',
                     label='Unrar to',
                     tip='Unrar to a chosen directory'
                 )
-                item2.connect('activate', self.unrar_to, safepath)
+                item2.connect('activate', self.unrar_to, filepath)
                 return [item1, item2]
             return None 
         return None 
